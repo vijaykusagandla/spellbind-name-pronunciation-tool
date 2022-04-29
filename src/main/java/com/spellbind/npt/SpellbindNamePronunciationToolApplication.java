b6e5d78233e525232b47bfce78a8167b021a9a29
Vijayakumar Reddy Kusagandla <vijayakumarreddy.kusagandla@gmail.com>
2022-04-29 20:09:17 +0530

    Initial Commit

---
+++ src/main/java/com/spellbind/npt/SpellbindNamePronunciationToolApplication.java
@@ -0,0 +1,13 @@
+package com.spellbind.npt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpellbindNamePronunciationToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpellbindNamePronunciationToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spellbind/npt/SpellbindNamePronunciationToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spellbind.npt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpellbindNamePronunciationToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
