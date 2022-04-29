74803a694b1d53930ada4f4bccae03da19cb6d2e
Vijayakumar Reddy Kusagandla <89499173+vijaykusagandla@users.noreply.github.com>
2022-04-29 20:10:34 +0530

    Update HealthCheckController.java

--- src/main/java/com/spellbind/npt/controller/HealthCheckController.java
+++ src/main/java/com/spellbind/npt/controller/HealthCheckController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
 
-	@GetMapping("/healthcheck")
+	@GetMapping("/healthCheck")
 	public String getDepartments() {
 		return "Application is working";
 	}
