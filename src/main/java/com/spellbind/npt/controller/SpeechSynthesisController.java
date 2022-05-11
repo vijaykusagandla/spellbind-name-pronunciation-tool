39f292e61114f75a3ad9c6d2efbe4b04ebdde709
Vijayakumar Reddy Kusagandla <vijayakumarreddy.kusagandla@gmail.com>
2022-05-11 15:35:41 +0530

    Update SpeechSynthesisController.java

--- src/main/java/com/spellbind/npt/controller/SpeechSynthesisController.java
+++ src/main/java/com/spellbind/npt/controller/SpeechSynthesisController.java
@@ -18,6 +18,8 @@ import com.microsoft.cognitiveservices.speech.SpeechSynthesisCancellationDetails
 import com.microsoft.cognitiveservices.speech.SpeechSynthesisResult;
 import com.microsoft.cognitiveservices.speech.SpeechSynthesizer;
 import com.microsoft.cognitiveservices.speech.audio.AudioConfig;
+import com.microsoft.cognitiveservices.speech.audio.AudioOutputStream;
+import com.microsoft.cognitiveservices.speech.audio.PullAudioOutputStream;
 import com.spellbind.npt.exception.NamePronunciationToolException;
 import com.spellbind.npt.model.InputText;
 
@@ -29,39 +31,46 @@ public class SpeechSynthesisController {
 	@GetMapping(path = "/speechSynthesis", consumes = "application/json")
 	public ResponseEntity<byte[]> speechSynthesis(@RequestBody InputText request) {
 
-		SpeechConfig speechConfig = SpeechConfig.fromSubscription("subscriptionKey", "eastus");
+		SpeechConfig speechConfig = SpeechConfig.fromSubscription("e80ee60bd3e440e2a189989ae3ed1327", "eastus");
 		speechConfig.setSpeechSynthesisVoiceName("en-US-JennyNeural"); // JennyNeural JennyMultiLingualNeural
 
-		String fileName = "outputaudio.wav";
-		AudioConfig fileOutput = AudioConfig.fromWavFileOutput(fileName);
-		byte[] file = null;
+		 // Creates an audio out stream.
+		PullAudioOutputStream stream = AudioOutputStream.createPullStream();
 
-		SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer(speechConfig, fileOutput);
+		 // Creates a speech synthesizer using audio stream output.
+		AudioConfig streamConfig = AudioConfig.fromStreamOutput(stream);
+
+		SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer(speechConfig, streamConfig);
 
 		// Get text from the console and synthesize to the default speaker.
-		SpeechSynthesisResult speechRecognitionResult = null;
+		SpeechSynthesisResult result = null;
+		byte[] audioData = null;
 		try {
-			speechRecognitionResult = speechSynthesizer.SpeakTextAsync(request.getText()).get();
+			result = speechSynthesizer.SpeakTextAsync(request.getText()).get();
+			audioData = result.getAudioData();
 		} catch (InterruptedException e) {
 			throw new NamePronunciationToolException("InterruptedException occured" + e);
 		} catch (ExecutionException e) {
 			throw new NamePronunciationToolException("ExecutionException occured" + e);
+		} finally {
+			result.close();
+			speechSynthesizer.close();
+			streamConfig.close();
+			stream.close();
+
 		}
-
-		if (speechRecognitionResult.getReason() == ResultReason.SynthesizingAudioCompleted) {
-			log.info("Speech synthesized to speaker for text [" + request.getText() + "] ");
-			file = speechRecognitionResult.getAudioData();
-
-		} else if (speechRecognitionResult.getReason() == ResultReason.Canceled) {
-			SpeechSynthesisCancellationDetails cancellation = SpeechSynthesisCancellationDetails
-					.fromResult(speechRecognitionResult);
+		// Checks result.
+		if (result.getReason() == ResultReason.SynthesizingAudioCompleted) {
+			log.info("Speech synthesized for text [" + request.getText()
+					+ "], and the audio was written to output stream.");
+		} else if (result.getReason() == ResultReason.Canceled) {
+			SpeechSynthesisCancellationDetails cancellation = SpeechSynthesisCancellationDetails.fromResult(result);
 			log.info("CANCELED: Reason=" + cancellation.getReason());
 
 			if (cancellation.getReason() == CancellationReason.Error) {
 				log.error("CANCELED: ErrorCode=" + cancellation.getErrorCode());
 				log.error("CANCELED: ErrorDetails=" + cancellation.getErrorDetails());
-				log.error("CANCELED: Did you set the speech resource key and region values?");
-				throw new NamePronunciationToolException("CANCELED: ErrorCode=" + cancellation.getErrorCode());
+				log.error("CANCELED: Did you update the subscription info?");
 			}
 		}
 
@@ -70,8 +79,8 @@ public class SpeechSynthesisController {
 		headers.add("Pragma", "no-cache");
 		headers.add("Expires", "0");
 
-		return ResponseEntity.ok().headers(headers).contentLength(file.length)
-				.contentType(MediaType.APPLICATION_OCTET_STREAM).body(file);
+		return ResponseEntity.ok().headers(headers).contentLength(audioData.length)
+				.contentType(MediaType.APPLICATION_OCTET_STREAM).body(audioData);
 	}
 
 }
