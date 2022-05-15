bb570fb56e3bd4ebfe677b91b06cf06f64639985
Suryakiran Allu <suryakiran.allu@gmail.com>
2022-05-15 15:59:04 +0530

    Adding CUSTOM_ELEMENTS_SCHEMA to ignore web components processing by angular

--- name-pronounciation-app/src/app/app.module.ts
+++ name-pronounciation-app/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AgGridModule } from 'ag-grid-angular';
@@ -22,6 +22,7 @@ import { SpeechSynthesisComponent } from './speech-synthesis/speech-synthesis.co
     ReactiveFormsModule,
     HttpClientModule
   ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   providers: [
     {
       provide: WindowToken,
